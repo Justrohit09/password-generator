fbd66e936d0afb1518854fb95fea3ae36f23c856
Justrohit09 <138568054+Justrohit09@users.noreply.github.com>
2024-09-30 22:59:31 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,52 +1,38 @@
-let inputEL=document.getElementById("input");
-let copyBtn=document.getElementById("btn2");
-let rangeSlider=document.getElementById('input-range');
-let displaylength=document.getElementById('display-length');
+let inputEL = document.getElementById("input");
+let copyBtn = document.getElementById("btn2");
+let rangeSlider = document.getElementById("input-range");
+let displaylength = document.getElementById("display-length");
 
 var currentValue;
-rangeSlider.addEventListener('input',(slide)=>{
-    currentValue=rangeSlider.value;
-    displaylength.textContent=rangeSlider.value;
-})
-
+rangeSlider.addEventListener("input", (slide) => {
+  currentValue = rangeSlider.value;
+  displaylength.textContent = rangeSlider.value;
+});
 
 // password generate
-let chars='123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXY';
-let char=chars.toUpperCase();
-var password='';
-let length=6;
-let generatePassword =()=>{
-    
-    for(let i=0;i<currentValue;i++){
-    password+=char.charAt(Math.floor(Math.random()*char.length))
-}
-return password
-}
+let char =
+  "123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXY";
+var password = "";
+let length = 6;
+let generatePassword = () => {
+  for (let i = 0; i < currentValue; i++) {
+    password += char.charAt(Math.floor(Math.random() * char.length));
+  }
+  return password;
+};
 
-
-
-
-
-let generateBtn=document.getElementById("btn1");
-generateBtn.addEventListener('click',()=>{
-    
-    inputEL.value=generatePassword();
-})
-
-
-
-
+let generateBtn = document.getElementById("btn1");
+generateBtn.addEventListener("click", () => {
+  inputEL.value = generatePassword();
+});
 
 // copy function
-const copyPassword=()=>{
-    var copyText=document.getElementById("input");
-    copyText.select();
-    copyText.setSelectionRange(0,length);
-    document.execCommand("copy");
-}
+const copyPassword = () => {
+  var copyText = document.getElementById("input");
+  copyText.select();
+  copyText.setSelectionRange(0, length);
+  document.execCommand("copy");
+};
 
-
-btn2.addEventListener('click',copyPassword)
-window.location.reload();
-
+btn2.addEventListener("click", copyPassword);
 
